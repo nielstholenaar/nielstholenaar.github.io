37ecd8479febf36b9b2bacbf5866d86e65fe63ef
Niels Tholenaar <info@123quality.nl>
2021-10-11 15:02:58 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+Array.from(document.querySelectorAll('.profile__arrow')).forEach(function (button) {
+  button.addEventListener('click', function (e) {
+    alert('Waarom verder zoeken?')
+  });
+});
