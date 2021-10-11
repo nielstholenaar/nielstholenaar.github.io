095ffb5900e769657f8bd2cb346219b12b616af0
Niels Tholenaar <info@123quality.nl>
2021-10-11 15:32:34 +0200

    Small changes

--- js/main.js
+++
@@ -1,5 +0,0 @@
-Array.from(document.querySelectorAll('.profile__arrow')).forEach(function (button) {
-  button.addEventListener('click', function (e) {
-    alert('Waarom verder zoeken?')
-  });
-});
